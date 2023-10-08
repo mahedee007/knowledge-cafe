58de90cd4a1ce729d0c3e4d6045487ec057570f1
Mahedee Hasan <mahedee007@gmail.com>
2023-10-08 22:57:15 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="nav">
+            
+        <h1>Learning World</h1>
+      < img src="./src/images/profile.png" alt="" />
+    
+        </div>
+    );
+};
+
+export default Header;
